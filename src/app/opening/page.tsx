aaad9e3ddcc981228ba91a43e05010ecff71af64
rucker122 <rucker122@gmail.com>
2025-05-26 17:26:44 +0900

    base file, animation

---
+++ src/app/opening/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import styles from "./page.module.css";
+import { useRouter } from "next/navigation";
+
+export default function Opening() {
+  return (
+    <div className={styles.content}>
+      <h1>Opening</h1>
+    </div>
+  );
+}
